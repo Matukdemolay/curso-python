479db134824eb32a9db32dfeff729876e33bbb06
Matukdemolay <mateusmatuck@gmail.com>
2024-08-27 21:38:30 -0300

    Feito meu primeiro desafio em python, utilizando a função 'time'

---
+++ exercicios/capitulo_01/desafio_01.py
@@ -0,0 +1,8 @@
+import time
+
+print("Hora do show!!")
+time.sleep(2)
+print("Se tornando um aprendiz da programação em python!")
+print("Aguarda 4,5 segundos, por gentileza.")
+time.sleep(4.5)
+print("Fim...")
