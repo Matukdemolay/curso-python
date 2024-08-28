da7e9a5fe771ac65dcf4eddc623f506e23e36d9f
Matukdemolay <mateusmatuck@gmail.com>
2024-08-28 00:55:02 -0300

    Consertando meu codigo onde o objetivo era printar as variaveis!

--- praticando/printando_variaveis.py
+++ praticando/printando_variaveis.py
@@ -1,6 +1,8 @@
+
+
 inteiro = 10 
 
-float = 4,5 
+decimal = 4.5 
 
 complexo = 5 + 7j
 
@@ -16,6 +18,7 @@ verdadeiro = True
 
 
 #O "f" antes das aspas significa que irei imprimir o conteudo das variaveis
+""""
 print(f'''
       Tipo Integer: {inteiro};  
       Tipo Decimal: {float};
@@ -25,3 +28,26 @@ print(f'''
       String Tripla: {string_tripla};
       Verdadeiro: {verdadeiro};
       Falso: {falso}''')
+      
+      """
+      
+      
+# Criando uma lista com cada linha formatada
+lines = [
+    f'Tipo Integer: {inteiro}',
+    f'Tipo Decimal: {decimal}',
+    f'Tipo Complexo: {complexo}',
+    f'String Simples: {string_simples}',
+    f'String Dupla: {string_dupla}',
+    f'String Tripla: {string_tripla}',
+    f'Verdadeiro: {verdadeiro}',
+    f'Falso: {falso}'
+]
+
+# Usando join() para concatenar as linhas com quebras de linha
+formatted_output = '\n      '.join(lines) 
+
+#Imprimindo o resultado
+print(f'''      
+      {formatted_output}
+''')  
