4112887a04371606551fa5ae656ec5998a259ad8
Matukdemolay <mateusmatuck@gmail.com>
2024-08-27 22:29:50 -0300

    Praticando algumas variaveis...

---
+++ praticando/variavel.py
@@ -0,0 +1,23 @@
+doce = 'brigadeiro'
+
+inteiro = 8
+
+decimal = 8.1
+
+#Numeros complexos em python sao representados pela letra J
+complexo = 5+2j 
+
+simples = 'Gosto de brigadeiro'
+
+duplas = "Gosto de brigadeiro"
+
+triplas = '''Quando a string tem mais de 
+uma linha'''
+
+verdadeiro = True
+falso = False 
+
+#A funcao type para descobrir o tipo de dado de cada variavel
+
+type(complexo)
+type(doce)
