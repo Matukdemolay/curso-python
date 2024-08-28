683e1ee0f4c9eb3fe1e7d75e48ccc978bed3fc78
Matukdemolay <mateusmatuck@gmail.com>
2024-08-27 23:16:34 -0300

    Exercicio do capitulo 2 concluido na pasta 'praticndo'

---
+++ praticando/printando_variaveis.py
@@ -0,0 +1,27 @@
+inteiro = 10 
+
+float = 4,5 
+
+complexo = 5 + 7j
+
+string_simples = 'Opa!!'
+
+string_dupla = "E ae!"
+
+string_tripla = '''Alou
+fala comigo rapazeada!'''
+
+falso = False
+verdadeiro = True
+
+
+#O "f" antes das aspas significa que irei imprimir o conteudo das variaveis
+print(f'''
+      Tipo Integer: {inteiro};  
+      Tipo Decimal: {float};
+      Tipo Complexo: {complexo};
+      String Simples: {string_simples};
+      String Dupla: {string_dupla};
+      String Tripla: {string_tripla};
+      Verdadeiro: {verdadeiro};
+      Falso: {falso}''')
